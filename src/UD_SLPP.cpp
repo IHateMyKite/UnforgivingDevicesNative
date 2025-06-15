d08709db0e148188adc3ef044e8372988e6de084
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2025-06-14 18:58:57 -0700

    strip up to 3 character prefix for some animation names

--- src/UD_SLPP.cpp
+++ src/UD_SLPP.cpp
@@ -16,31 +16,40 @@ namespace UD
             if (ptr) {
                 std::string lowerOriginalAnimationName(originalAnimationName);
                 std::transform(lowerOriginalAnimationName.begin(), lowerOriginalAnimationName.end(), lowerOriginalAnimationName.begin(), [] (char c) { return std::tolower(c);});
+                
                 for (auto graph : ptr->graphs) {
-                    if (!graph->behaviorGraph) {
+                    if (!graph) {
                         continue;
                     }
-                    if (!graph->behaviorGraph->data) {
-                        continue;    
-                    }
-                    if (!graph->behaviorGraph->data->stringData) {
-                        continue;
-                    }
-                    for (auto eventName: graph->behaviorGraph->data->stringData->eventNames) {
-                        std::string lowerEventName(eventName.c_str());
-                        std::transform(lowerEventName.begin(), lowerEventName.end(), lowerEventName.begin(), [] (char c) { return std::tolower(c);});
-                        
-                        auto found=lowerEventName.find(lowerOriginalAnimationName);
-                        
-                        if (found != std::string::npos) {
-                            std::string eventName2(eventName.c_str());
-                            if (found+lowerOriginalAnimationName.size() == lowerEventName.size())
-                            {
-                                
-                                SKSE::log::info("animation {}",eventName2);
-                                return eventName2;
-                            } else {
-                                SKSE::log::info("incorrect animation {}",eventName2);
+                    
+                        if (!graph->characterInstance.setup) {
+                            continue;
+                        }
+                        if (!graph->characterInstance.setup->data) {
+                            continue;
+                        }
+                        if (!graph->characterInstance.setup->data->stringData) {
+                            continue;
+                        }
+                    for (int prefix_removal=0;prefix_removal<4; prefix_removal+=1) {
+                        for (auto eventBundle : graph->characterInstance.setup->data->stringData->animationNames) {
+                            auto eventName = eventBundle.bundleName;
+                            std::string lowerEventName(eventName.c_str());
+                            std::transform(lowerEventName.begin(), lowerEventName.end(), lowerEventName.begin(), [] (char c) { return std::tolower(c);});
+                            
+                            if (lowerEventName.size() > prefix_removal+1) {
+                                auto found = lowerEventName.find(lowerOriginalAnimationName.substr(prefix_removal));
+
+                                if (found != std::string::npos) {
+                                    std::string eventName2(eventName.c_str());
+                                    if (found + (lowerOriginalAnimationName.size()-prefix_removal) == lowerEventName.size()) {
+                                        SKSE::log::info("animation {}", eventName2);
+                                        return eventName2;
+                                    } else {
+                                        SKSE::log::info("incorrect animation {}", eventName2);
+                                    }
+                                }
+                                prefix_removal += 1;
                             }
                         }
                     }
