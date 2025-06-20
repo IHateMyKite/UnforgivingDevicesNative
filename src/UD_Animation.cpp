3a5d56b361d448761345c27443d8f00db36bd04e
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2025-06-19 21:27:41 -0700

    try to fix filtering again

--- src/UD_Animation.cpp
+++ src/UD_Animation.cpp
@@ -356,9 +356,9 @@ std::vector<std::string> UD::AnimationManager::GetAnimationsFromDB(std::string a
                             if (a_ActorConstraints.size() == 2) {
                                 constraints2=a_ActorConstraints[1];
                             }
-                            if ((a_ActorConstraints.size() == 1 || a_ActorConstraints.size() == 0) && GetAnimationsFromJSON(name+":"+loc_anim_path,{RE::PlayerCharacter::GetSingleton()->As<RE::Actor>()},constraints1,constraints2).size() > 2) {
+                            if ((a_ActorConstraints.size() == 1 || a_ActorConstraints.size() == 0) && GetAnimationsFromJSON(name+":"+loc_anim_path,{RE::PlayerCharacter::GetSingleton()->As<RE::Actor>()},constraints1,constraints2).size() >= 2) {
                                 loc_result.push_back(name + ":" + loc_anim_path);
-                            } else if (a_ActorConstraints.size() == 2 && GetAnimationsFromJSON(name+":"+loc_anim_path,{RE::PlayerCharacter::GetSingleton()->As<RE::Actor>(),RE::PlayerCharacter::GetSingleton()->As<RE::Actor>()},constraints1,constraints2).size() > 2) {
+                            } else if (a_ActorConstraints.size() == 2 && GetAnimationsFromJSON(name+":"+loc_anim_path,{RE::PlayerCharacter::GetSingleton()->As<RE::Actor>(),RE::PlayerCharacter::GetSingleton()->As<RE::Actor>()},constraints1,constraints2).size() >= 2) {
                                 loc_result.push_back(name + ":" + loc_anim_path);
                             }
                             
@@ -634,6 +634,7 @@ bool UD::AnimationManager::_CheckConstraints(boost::json::value a_obj, std::stri
             if ((~(loc_anim_optConstr|loc_anim_reqConstr) & a_ActorConstraints)==0) {
                 return true;
             }
+            // TODO: sort by most optional constraints
             return false;
         }
     }
